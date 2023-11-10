6efa15be411f048eff8d706b2f9a0eb8debcefbb
enricoforesta <enrico.foresta97@gmail.com>
2023-11-10 11:21:26 +0100

    feat: scaffolding

---
+++ snack1/main.js
@@ -0,0 +1,6 @@
+const elementPrimoInput = [prompt("inserisci una parola")];
+const elementSecondoInput = [prompt("inserisci una parola")];
+
+console.log({elementPrimoInput});
+console.log({elementSecondoInput});
+
