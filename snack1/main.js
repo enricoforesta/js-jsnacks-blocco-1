7ec115236b44720bf78ce8aadcd4d6f1b5cacf4d
enricoforesta <enrico.foresta97@gmail.com>
2023-11-10 11:22:15 +0100

    feat: scaffolding

--- snack1/main.js
+++ snack1/main.js
@@ -1,6 +1,18 @@
 const elementPrimoInput = [prompt("inserisci una parola")];
 const elementSecondoInput = [prompt("inserisci una parola")];
 
-console.log({elementPrimoInput});
-console.log({elementSecondoInput});
+console.log({ elementPrimoInput });
+console.log({ elementSecondoInput });
+
+if (elementPrimoInput.length > elementSecondoInput.length) {
+    console.log(elementPrimoInput);
+    console.log(elementSecondoInput);
+}
+else if (elementPrimoInput.length < elementSecondoInput.length) {
+    console.log(elementSecondoInput);
+    console.log(elementPrimoInput);
+}
+else{
+    console.log("Le parole hanno la stessa lugnhezza")
+}
 
